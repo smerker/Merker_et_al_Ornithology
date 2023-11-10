1fdf292b1b2132bec612c3314f041b32f72f30e0
smerker <sam.merker@gmail.com>
2023-11-10 16:29:51 -0500

    Update Describing_nest_climate_analyses.R
    Now comes with AIC creation

--- Describing_nest_climate_analyses.R
+++ Describing_nest_climate_analyses.R
@@ -1,14 +1,21 @@
 #### Load data for analyses ####
 
 load("Nest_Climate_Data.gzip")
-#save(night.sum.stats, night.out.sum.stats, day.sum.stats, day.out.sum.stats, file="Nest_Climate_Data.gzip")
+#save(night.sum.stats, night.out.sum.stats, day.sum.stats, day.out.sum.stats, file="D:/Dissertation/Dissertation/Egg Swapping/Writing/Functional Ecology/EggTrans_Ornith_R_proj/Nest_Climate_Data_new.gzip")
+
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 #### Night time Temperature and Humidity Analysis ####
 
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 library(multcomp)
 
-##Night time temp inside nests.
+##Night time temperature inside nests.
 
 ## species only
 lm.sp.t.in<-lm(meanT~species,data=night.sum.stats)
@@ -20,7 +27,10 @@ summary(lm.elev.t.in)
 
 ## elevation + species
 lm.elev.sp.t.in<-lm(meanT~elevation+species, data=night.sum.stats)
-summary(lm.elev.sp.t.in)
+summary(anova(lm.elev.sp.t.in))
+
+# effect size
+mean(night.sum.stats$meanT[night.sum.stats$species=="HOWA"])-mean(night.sum.stats$meanT[night.sum.stats$species=="BTBW"])
 
 ## interaction
 lm.t.in.X<-lm(meanT~elevation*species, data=night.sum.stats)
@@ -39,6 +49,13 @@ colnames(t.AIC.df)<-c("K", "AIC", "deltaAIC", "weight")
 t.AIC.df.ord<-round(t.AIC.df[order(t.AICdff$AIC, decreasing =F),],3)
 t.AIC.df.ord
 
+rownames(t.AIC.df.ord)<-c("Mean Internal Temp ~ Elevation", 
+                          "Mean Internal Temp ~ Species",
+                          "Mean Internal Temp ~ Species  X Elevation",
+                          "Mean Internal Temp ~ Elevation + Species")
+
+write.csv(t.AIC.df.ord,"Nightime_Temp_Inside_Nests.csv")
+
 #
 ##make predictions from model with species + elevation
 #
@@ -50,10 +67,13 @@ new.dat.in.temp<-rbind(new.dat.in.temp,howa.temp)
 
 #make predictions
 pred.temp.in3<-predict(lm.elev.sp.t.in, newdata=new.dat.in.temp, se.fit=TRUE, type="response", interval="confidence")
+summary(lm.elev.sp.t.in)
 
 new.dat.in.temp$pred.fit<-pred.temp.in3$fit[,1]
 new.dat.in.temp$pred.CI.lwr<-pred.temp.in3$fit[,2]
 new.dat.in.temp$pred.CI.upr<-pred.temp.in3$fit[,3]
+
+
 
 
 #
@@ -63,7 +83,6 @@ new.dat.in.temp$pred.CI.upr<-pred.temp.in3$fit[,3]
 ## temperature
 #
 #
-
 
 #make species a factor for Tukey
 night.out.sum.stats$species<-as.factor(night.out.sum.stats$species)
@@ -78,8 +97,29 @@ lm.elev.t.out<-lm(meanT~elevation, data=night.out.sum.stats)
 summary(lm.elev.t.out)
 
 
+lm.elev.sp.t.out<-lm(meanT~elevation+species, data=night.out.sum.stats)
+summary(lm.elev.sp.t.out)
+
+# effect size
+#mean(night.out.sum.stats$meanT[night.out.sum.stats$species=="HOWA"])-mean(night.out.sum.stats$meanT[night.out.sum.stats$species=="BTBW"])
+
+AIC(lm.elev.t.out,lm.elev.sp.t.out)
 #new data frame for predictions
 new.dat.out.temp<-data.frame(elevation=seq(735, 1519, by=10))
+
+new.dat.in.temp.out<-data.frame(elevation=seq(1200, 1519, by=10), species="BTBW")#, height=c(.78,.99))#average nest height for each species
+howa.temp.out<-data.frame(elevation=seq(735, 1200, by=10), species="HOWA")
+new.dat.in.temp.out<-rbind(new.dat.in.temp.out,howa.temp)
+
+
+
+pred.temp.out.e.sp<-predict(lm.elev.sp.t.out, newdata=new.dat.in.temp.out, se.fit=TRUE, type="response", interval="confidence")
+new.dat.in.temp.out$pred.fit<-pred.temp.out.e.sp$fit[,1]
+new.dat.in.temp.out$pred.CI.lwr<-pred.temp.out.e.sp$fit[,2]
+new.dat.in.temp.out$pred.CI.upr<-pred.temp.out.e.sp$fit[,3]
+
+plot(pred.fit~elevation, data=subset(new.dat.in.temp.out, species=="BTBW"), type='l', ylim=c(13, 20), xlim=c(700, 1550))
+lines(pred.fit~elevation, data=subset(new.dat.in.temp.out, species=="HOWA"), col='red')
 
 #make predictions
 pred.temp.out3<-predict(lm.elev.t.out, newdata=new.dat.out.temp, se.fit=TRUE, type="response", interval="confidence")
@@ -120,6 +160,7 @@ summary(log.elev.sp.hu.in)
 # interaction
 log.hu.in.X<-lm(logit(meanHprop)~elevation*species, data=night.sum.stats)
 summary(log.hu.in.X)
+coef(log.hu.in.X)
 
 # model selection
 logh.AIC<-AIC(log.sp.hu.in, log.elev.hu.in, log.elev.sp.hu.in,log.hu.in.X)
@@ -132,6 +173,14 @@ colnames(logh.AIC.df)<-c("K", "AIC", "deltaAIC", "weight")
 logh.AIC.df.ord<-round(logh.AIC.df[order(logh.AICdff$AIC),],3)
 logh.AIC.df<-logh.AIC.df.ord
 logh.AIC.df
+
+rownames(logh.AIC.df)<-c("Mean Internal Humid ~ Species  X Elevation", 
+                         "Mean Internal Humid ~ Species",
+                         "Mean Internal Humid ~ Elevation",
+                         "Mean Internal Humid ~ Elevation + Species")
+
+write.csv(logh.AIC.df,"Nightime_Humid_Inside_Nests.csv")
+
 
 #
 ##make predictions
@@ -162,6 +211,7 @@ new.dat.loghumid
 log.elev.hu.out<-lm(logit(meanHprop)~elevation, data=night.out.sum.stats)
 summary(log.elev.hu.out)
 
+summary(log.elev.hu.out)$coefficients[, "Std. Error"]
 #new data frame for predictions
 new.dat.loghumid.out<-data.frame(elevation=seq(735, 1519, by=10)) #average nest height for each species
 
@@ -184,20 +234,20 @@ new.dat.loghumid.out$pred.CI.upr<-plogis(pred.loghumid.out3$fit[,3])
 
 # inner daytime temperature
 
-day.in.mod.sp<-lm(meanT~species, data=day.sum.stats)
-day.in.mod.x<-lm(meanT~elevation*species, data=day.sum.stats)
-day.in.mod.s.e<-lm(meanT~species+elevation, data=day.sum.stats)
-dayu.in.mod.e<-lm(meanT~elevation, data=day.sum.stats)
+day.in.T.mod.sp<-lm(meanT~species, data=day.sum.stats)
+day.in.T.mod.x<-lm(meanT~elevation*species, data=day.sum.stats)
+day.in.T.mod.s.e<-lm(meanT~species+elevation, data=day.sum.stats)
+day.in.T.mod.e<-lm(meanT~elevation, data=day.sum.stats)
 
 
-summary(day.in.mod.sp)
-summary(day.in.mod.x)
-summary(day.in.mod.s.e)
-summary(dayu.in.mod.e)
+summary(day.in.T.mod.sp)
+summary(day.in.T.mod.x)
+summary(day.in.T.mod.s.e)
+summary(day.in.T.mod.e)
 
 
 #model selection
-t.AIC.day<-AIC(day.in.mod.sp, day.in.mod.x, day.in.mod.s.e, dayu.in.mod.e)
+t.AIC.day<-AIC(day.in.T.mod.sp, day.in.T.mod.x, day.in.T.mod.s.e, day.in.T.mod.e)
 str(t.AIC.day)
 t.AICdff.day<-t.AIC.day[2]-min(t.AIC.day[,2])
 w.day <- exp(-0.5*t.AICdff.day)/sum(exp(-0.5*t.AICdff.day))
@@ -207,6 +257,15 @@ colnames(t.AIC.df.day)<-c("K", "AIC", "deltaAIC", "weight")
 
 t.AIC.df.day.ord<-round(t.AIC.df.day[order(t.AICdff.day$AIC),],3)
 t.AIC.df.day.ord
+
+rownames(t.AIC.df.day.ord)<-c("Mean Internal Temp ~ Elevation", 
+                              "Mean Internal Temp ~ Species",
+                              "Mean Internal Temp ~ Elevation X Species",
+                              "Mean Internal Temp ~ Elevation + Species")
+
+write.csv(t.AIC.df.day.ord,"Daytime_Temp_Inside_Nests.csv")
+
+
 min(day.sum.stats$elevation[day.sum.stats$species=="BTBW"], na.rm=T)
 
 #new data frame for predictions
@@ -242,7 +301,7 @@ summary(lm.elev.dayt.out)
 #humid~elevation
 lm.elev.dayh.out<-lm(logit(meanHprop)~elevation, data=day.out.sum.stats)
 summary(lm.elev.dayh.out)
-
+summary(lm.elev.dayh.out)$coefficients[, "Std. Error"]
 #new data frame for predictions
 new.dat.out.daytemp<-data.frame(elevation=seq(739, 1519, by=10))
 
@@ -271,17 +330,17 @@ new.dat.out.daytemp$pred.CI.H.upr<-plogis(pred.dayhumid.out3$fit[,3])
 day.in.mod.sp.h<-lm(logit(meanH/100)~species, data=day.sum.stats)
 day.in.mod.x.h<-lm(logit(meanH/100)~elevation*species, data=day.sum.stats)
 day.in.mod.s.e.h<-lm(logit(meanH/100)~species+elevation, data=day.sum.stats)
-dayu.in.mod.e.h<-lm(logit(meanH/100)~elevation, data=day.sum.stats)
+day.in.mod.e.h<-lm(logit(meanH/100)~elevation, data=day.sum.stats)
 
 
 summary(day.in.mod.sp.h)
 summary(day.in.mod.x.h)
 summary(day.in.mod.s.e.h)
-summary(dayu.in.mod.e.h)
+summary(day.in.mod.e.h)
 
 
 #which one was better
-h.AIC.day<-AIC(day.in.mod.sp.h, day.in.mod.x.h, day.in.mod.s.e.h, dayu.in.mod.e.h)
+h.AIC.day<-AIC(day.in.mod.sp.h, day.in.mod.x.h, day.in.mod.s.e.h, day.in.mod.e.h)
 str(h.AIC.day)
 h.AICdff.day<-h.AIC.day[2]-min(h.AIC.day[,2])
 hw.day <- exp(-0.5*h.AICdff.day)/sum(exp(-0.5*h.AICdff.day))
@@ -291,6 +350,15 @@ colnames(h.AIC.df.day)<-c("K", "AIC", "deltaAIC", "weight")
 
 h.AIC.df.day.ord<-round(h.AIC.df.day[order(h.AICdff.day$AIC),],3)
 h.AIC.df.day.ord
+
+
+rownames(h.AIC.df.day.ord)<-c("Mean Internal Humid ~ Elevation", 
+                              "Mean Internal Humid ~ Species",
+                              "Mean Internal Humid ~ Elevation X Species",
+                              "Mean Internal Humid ~ Elevation + Species")
+
+write.csv(h.AIC.df.day.ord,"Daytime_Humid_Inside_Nests.csv")
+
 
 
 #new data frame for predictions
@@ -323,8 +391,6 @@ new.dat.in.hum.day$pred.CI.upr<-plogis(pred.humid.in2.day$fit[,3])
 # exterior daytime humidity
 
 
-
-
 lm.elev.dayh.out<-lm(logit(meanHprop)~elevation, data=day.out.sum.stats)
 summary(lm.elev.dayh.out)
 
@@ -349,6 +415,5 @@ new.dat.out.daytemp$pred.CI.H.lwr<-plogis(pred.dayhumid.out3$fit[,2])
 #model averaged upr CI
 new.dat.out.daytemp$pred.CI.T.upr<-pred.daytemp.out3$fit[,3]
 new.dat.out.daytemp$pred.CI.H.upr<-plogis(pred.dayhumid.out3$fit[,3])
-
 
 
