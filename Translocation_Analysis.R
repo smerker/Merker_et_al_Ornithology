4d04476e06435fc71e5b0074cb8c2ba76582663a
smerker <sam.merker@gmail.com>
2023-11-10 16:16:08 -0500

    Update Translocation_Analysis.R
    removed unused plotting code

--- Translocation_Analysis.R
+++ Translocation_Analysis.R
@@ -73,26 +73,3 @@ system("open hatch_rate.png")
 
 
 
-png("hatch_rate.png", width=5, height=4, units="in", res=800)
-par(mar=c(4,5,1,1), oma=c(0,0,0,0))
-
-barplot(new.dat$prediction[c(1,3,5,2,4,6)], ylim=c(0.4,1), space=c(0,0,0,.3,0,0), 
-        col=c("lightblue","lightblue","lightblue", "orangered", "orangered", "orangered"), ylab="Hatch Rate", yaxt='n', xpd=F)
-
-axis(side=2, at=c(.4,.6,.8,1), labels = FALSE)
-axis.break(2, 0.5, style = "slash", brw=.06)
-text(par("usr")[1],c(0.4,.6,.8,1),
-     labels = ytick, pos = 2, xpd=NA, offset=1)
-
-arrows(c(.5,1.5,2.5,3.8,4.8,5.8), plogis(pred1$fit[c(1,3,5,2,4,6)]), 
-       c(.5,1.5,2.5,3.8,4.8,5.8), plogis(pred1$fit[c(1,3,5,2,4,6)])+new.dat$se[c(1,3,5,2,4,6)], code=3, angle=90, length=.05)
-text(c(.3,1.15,1.95,3.6,4.45,5.15)-.1, par("usr")[3]-c(.02,.02,.04), labels = labs, srt = 30, pos = 1, xpd = TRUE, cex=.9, font=1)
-#legend(-.2,.98, c("Black-throated blue", "Hooded"),title="Warbler Species", pch=15, col=c("lightblue","orangered"), cex=0.65)
-box(which="plot", bty=c("]"))
-
-
-dev.off()
-system("open hatch_rate.png")
-
-
-
